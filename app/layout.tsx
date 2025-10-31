f33c0c380a664ba5f99b6c5a63b7436bc634afd1
ethanescalante <ethan65g@gmail.com>
2025-10-30 17:24:32 -0700

    map added as component and displaying

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-            crossOrigin=""/>
       <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
