9115ce72a222b80fd60e94e09c04bdc9e356b4aa
ethanescalante <ethan65g@gmail.com>
2025-11-05 17:06:59 -0800

    added a surprise to the home button

--- app/CustomUI/Button.tsx
+++ app/CustomUI/Button.tsx
@@ -1,6 +1,8 @@
 import { ButtonGroupItem } from "@/components/base/button-group/button-group";
-import React from "react";
-
+import React, {useEffect} from "react";
+import { motion } from "framer-motion";
+import dogImage from "../../public/dog.png"
+import Image from "next/image";
 type ButtonProps = {
     picture?: React.ReactNode; // JSX.Element also ok
     id: string;
@@ -9,19 +11,46 @@ type ButtonProps = {
 };
 
 const Button = ({ picture, id, buttonName, homeButton }: ButtonProps) => {
+    const audio = React.useMemo(() => new Audio("/dogBarking.mp3"), []);
+    const play = () => { audio.play() };
+
     return (
-        <ButtonGroupItem
-            className={
-            !homeButton ?
-                "hover:bg-amber-200 selected:bg-amber-500 selected:font-bold"
-                :
-                ""
-        }
-            iconTrailing={picture}
-            id={id}
-        >
-            {buttonName}
-        </ButtonGroupItem>
+            <ButtonGroupItem
+                className="relative overflow-visible hover:bg-amber-200 selected:bg-amber-500 selected:font-bold group  "
+                iconTrailing={picture}
+                id={id}
+                {...(homeButton ? { onClickCapture: play } : {})}
+            >
+                {buttonName}
+
+                {homeButton && (
+                    <div
+                        className="
+                        absolute left-1/2
+                        -translate-x-1/2
+                        bottom-6
+                        opacity-0
+                        translate-y-0
+                        transition-all
+                        duration-500
+                        ease-out
+                        group-hover:opacity-100
+                        group-hover:-translate-y-2
+                        pointer-events-none
+                        "
+                    >
+                        <Image
+                            src={dogImage}
+                            alt="Dog"
+                            width={50}
+                            height={50}
+                            className="drop-shadow-lg"
+                        />
+                    </div>
+                )}
+
+            </ButtonGroupItem>
+
     );
 };
 
